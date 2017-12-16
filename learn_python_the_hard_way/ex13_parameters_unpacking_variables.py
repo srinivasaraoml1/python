2207b5e456968fc159edb396fe517508856c0f5c
Srinivasaimandi <srinivasaimandi@gmail.com>
2017-12-16 11:42:13 +0530

    ex13
    ex13_completed

---
+++ learn_python_the_hard_way/ex13_parameters_unpacking_variables.py
@@ -0,0 +1,10 @@
+# This is an exercise on "Parameters, Unpacking, Variables"
+
+from sys import argv
+
+script, first, second, third = argv
+
+print("This Script is called:", script)
+print("The Company is:", first)
+print("The Community is:", second)
+print("The goal of this test script is:", third)
