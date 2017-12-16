925a418c707ae3769d45b2f8690f3799d87c2080
Srinivasaimandi <srinivasaimandi@gmail.com>
2017-12-16 12:23:16 +0530

    ex15 edited with comments
    As per the instructions in ex16

--- learn_python_the_hard_way/ex15_reading_files.py
+++ learn_python_the_hard_way/ex15_reading_files.py
@@ -4,13 +4,18 @@ from sys import argv
 
 sript, file_name = argv
 
+# opens the text file into a file variable 'txt'
 txt = open(file_name)
 
+print(type(txt))
 print("Here's your file: %r" %file_name)
+# reading the file variable 'txt'
 print(txt.read())
 
 print "Type the filename again:"
 file_again = raw_input("> ")
 
+# opens the text file into a file variable 'txt_again'
 txt_again = open(file_again)
+# reading the file variable 'txt_again'
 print txt_again.read()
