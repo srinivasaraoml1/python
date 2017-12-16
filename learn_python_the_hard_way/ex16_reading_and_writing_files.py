c8cb696fa3e05a7694bb9a5b29d90b3b618e285f
Srinivasaimandi <srinivasaimandi@gmail.com>
2017-12-16 12:44:53 +0530

    Ex16 started
    ex16 in the process

--- learn_python_the_hard_way/ex16_reading_and_writing_files.py
+++ learn_python_the_hard_way/ex16_reading_and_writing_files.py
@@ -1,1 +1,25 @@
 # This is an exercise on "Reading And Writing Files"
+
+from sys import argv
+
+sript, file_name = argv
+
+print "We're going to erase %r." % file_name
+print "If you don't want that, hit CTRL-C (^C)."
+print "If you do want that, hit RETURN."
+raw_input("?")
+
+print("Opening the file")
+target = open(file_name, w)
+
+print("Truncating the file, Goodbye!")
+target.truncate()
+
+print "Now I'm going to ask you for three lines."
+line1 = raw_input("line 1: ")
+line2 = raw_input("line 2: ")
+line3 = raw_input("line 3: ")
+
+print "I'm going to write these to the file."
+target.write(line1)
+target.write("\n")
