ce47b04ae6720c6c9f084cb6b618ee4367881fc3
Srinivasaimandi <srinivasaimandi@gmail.com>
2017-12-16 12:51:22 +0530

    Ex16 completed
    Stats:completed

--- learn_python_the_hard_way/ex16_reading_and_writing_files.py
+++ learn_python_the_hard_way/ex16_reading_and_writing_files.py
@@ -10,16 +10,21 @@ print "If you do want that, hit RETURN."
 raw_input("?")
 
 print("Opening the file")
-target = open(file_name, w)
+# opens file in write mode
+target = open(file_name, 'w')
 
 print("Truncating the file, Goodbye!")
+# truncates the opened file uder the file variable 'target'
 target.truncate()
 
 print "Now I'm going to ask you for three lines."
+# entering the sample text in different lines
 line1 = raw_input("line 1: ")
 line2 = raw_input("line 2: ")
 line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
-target.write(line1)
-target.write("\n")
+# writing the entered lines in a sequence into the file opened & truncated initially
+target.write(line1+"\n")
+target.write(line2+"\n")
+target.write(line3+"\n")
