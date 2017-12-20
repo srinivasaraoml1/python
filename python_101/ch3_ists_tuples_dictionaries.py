419bf55217b28261d979d73faa18bae4766bdfe8
Srinivasaimandi <srinivasaimandi@gmail.com>
2017-12-21 02:19:40 +0530

    Lists, Tuples and Dictionaries Completed

--- python_101/ch3_ists_tuples_dictionaries.py
+++ python_101/ch3_ists_tuples_dictionaries.py
@@ -1,6 +1,6 @@
 # This is an exercise on "Lists, Tuples and Dictionaries"
 
-# Lists
+# Lists : Research more on List slicing
 # Setting up an empty or dummy list
 data = []
 names = list()
@@ -16,3 +16,26 @@ print(data1)
 
 # list functions
 print(names + dnames)
+
+dnames.sort()
+print(dnames)
+
+# Tuples
+my_tuple=(1,2,3,4,5)
+print(my_tuple[0:3])
+another_tuple = tuple()
+abc = tuple([1, 2, 3])
+print(abc)
+print(list(abc))
+
+# Dictionaries : These are basically a Hash Map[Key, Value Pairs]
+dict = {}
+print(dict)
+dict1 = dict
+print(dict1)
+dict2 = {'a':'1', 'b':'2', 'c':'3'}
+print(dict2)
+
+print('a' in  dict2)
+
+print(dict2.keys())
