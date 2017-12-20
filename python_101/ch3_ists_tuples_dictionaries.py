b6ffa923f75ed9ecb5c7c89bb247f13da9cf39ec
Srinivasaimandi <srinivasaimandi@gmail.com>
2017-12-21 02:09:58 +0530

    Completed

---
+++ python_101/ch3_ists_tuples_dictionaries.py
@@ -0,0 +1,18 @@
+# This is an exercise on "Lists, Tuples and Dictionaries"
+
+# Lists
+# Setting up an empty or dummy list
+data = []
+names = list()
+
+# Lists with data
+numbers = [1,2,3,4,5,6]
+print(numbers)
+dnames = ["Srinivas", "Baskar", "Sangameswari", "Shyamala", "Sowmya", "Praveena"]
+print(dnames)
+# Nested Lists
+data1 = [numbers, dnames]
+print(data1)
+
+# list functions
+print(names + dnames)
